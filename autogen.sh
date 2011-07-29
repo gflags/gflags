b7ea0659625be5cf8c9c3954ea2fe28f39300f81
Craig Silverstein <csilvers+gflags@google.com>
2011-07-29 03:48:59 +0000

    Update to automake 1.10.1 (from 1.9), and downdate to autoconf 2.62 (from 2.65).  I'm moving to a new system for maintaining the opensource version, which involves using a 'hermetic' version of autotools, and those are the versions installed in the hermetic system.  There shouldn't be a user-noticeable change here.
    git-svn-id: https://gflags.googlecode.com/svn/trunk@51 6586e3c6-dcc4-952a-343f-ff74eb82781d

--- autogen.sh
+++ autogen.sh
@@ -7,48 +7,23 @@
 # These are the files that this script might edit:
 #    aclocal.m4 configure Makefile.in src/config.h.in \
 #    depcomp config.guess config.sub install-sh missing mkinstalldirs \
-#    ltmain.sh
 #
 # Here's a command you can run to see what files aclocal will import:
 #  aclocal -I ../autoconf --output=- | sed -n 's/^m4_include..\([^]]*\).*/\1/p'
 
+# Because libtoolize isn't in the hermetic build, autogen doesn't run it.
+# However, it should be run manually periodically to update these files:
+#    in .: ltmain.sh
+#    in m4: libtool.m4 ltoptions.m4 ltsugar.m4 ltversion.m4 lt~obsolete.m4
+
 set -ex
 rm -rf autom4te.cache
 
-trap 'rm -f aclocal.m4.tmp' EXIT
-
-# Returns the first binary in $* that exists, or the last arg, if none exists.
-WhichOf() {
-  for candidate in "$@"; do
-    if "$candidate" --version >/dev/null 2>&1; then
-      echo "$candidate"
-      return
-    fi
-  done
-  echo "$candidate"   # the last one in $@
-}
-
-# Use version 1.9 of aclocal and automake if available.
-ACLOCAL=`WhichOf aclocal-1.9 aclocal`
-AUTOMAKE=`WhichOf automake-1.9 automake`
-LIBTOOLIZE=`WhichOf glibtoolize libtoolize15 libtoolize14 libtoolize`
-
-# aclocal tries to overwrite aclocal.m4 even if the contents haven't
-# changed, which is annoying when the file is not open for edit (in
-# p4).  We work around this by writing to a temp file and just
-# updating the timestamp if the file hasn't change.
-"$ACLOCAL" --force -I m4 --output=aclocal.m4.tmp
-if cmp aclocal.m4.tmp aclocal.m4; then
-  touch aclocal.m4               # pretend that we regenerated the file
-  rm -f aclocal.m4.tmp
-else
-  mv aclocal.m4.tmp aclocal.m4   # we did set -e above, so we die if this fails
-fi
-
-grep -q '^[^#]*AC_PROG_LIBTOOL' configure.ac && "$LIBTOOLIZE" -c -f
+aclocal --force -I m4
+#grep -q LIBTOOL configure.ac && libtoolize -c -f
 autoconf -f -W all,no-obsolete
 autoheader -f -W all
-"$AUTOMAKE" -a -c -f -W all
+automake -a -c -f -W all
 
 rm -rf autom4te.cache
 exit 0
