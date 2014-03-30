dc8543a4738548dda6f210cca035693e19c7042d
Andreas Schuh <andreas.schuh.84@gmail.com>
2014-03-30 15:16:00 +0100

    Add test which uses gflags_declare.h.
    Update issue 79
    Added a test which uses gflags_declare.h as any other project would use it to avoid such avoidable build configuration mistakes.

---
+++ test/gflags_declare_flags.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <gflags/gflags_declare.h>
+
+DECLARE_string(message); // in gflags_delcare_test.cc
+
+void print_message()
+{
+  std::cout << FLAGS_message << std::endl;
+}
