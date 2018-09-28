04c8ce76e243c1d99989b06d0376c0f6b911e460
Andreas Schuh <andreas.schuh.84@gmail.com>
2018-09-28 15:00:43 +0100

    fix: Indentation, use spaces instead of real tabs

--- src/gflags.cc
+++ src/gflags.cc
@@ -1036,7 +1036,7 @@ uint32 CommandLineFlagParser::ParseNewCommandLineFlags(int* argc, char*** argv,
     char* arg = (*argv)[i];
 
     // Like getopt(), we permute non-option flags to be at the end.
-	if (arg[0] != '-' || arg[1] == '\0') {	// must be a program argument "-" is an argument, not a flag
+    if (arg[0] != '-' || arg[1] == '\0') {	// must be a program argument "-" is an argument, not a flag
       memmove((*argv) + i, (*argv) + i+1, (*argc - (i+1)) * sizeof((*argv)[i]));
       (*argv)[*argc-1] = arg;      // we go last
       first_nonopt--;              // we've been pushed onto the stack
